f18dbc9328adfe2bf4c25efc1fadf1e6aad23077
venkatp227 <venkatp227@gmail.com>
2018-05-14 11:18:47 +0100

    my changes

--- main.tf
+++ main.tf
@@ -1,29 +1,34 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region     = "${var.region}"
+  profile    = "${var.profile}"
 }
 
 data "terraform_remote_state" "tfstate" {
   backend = "s3"
+
   config {
-    bucket = "mycompany-terraform"
-    key = "jenkins/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "bg-tfstate-1675"
+    key     = "jenkins/terraform.tfstate"
+    region  = "eu-west-2"
+    profile = "mytf"
   }
 }
 
 resource "aws_vpc" "jenkins" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 
   tags {
-    for = "${var.ecs_cluster_name}"
+    for  = "${var.ecs_cluster_name}"
+    Name = "ecs-jenkins-vpc"
   }
 }
 
 resource "aws_route_table" "external" {
   vpc_id = "${aws_vpc.jenkins.id}"
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.jenkins.id}"
@@ -34,18 +39,33 @@ resource "aws_route_table" "external" {
   }
 }
 
-resource "aws_route_table_association" "external-jenkins" {
-  subnet_id = "${aws_subnet.jenkins.id}"
+resource "aws_route_table_association" "external-jenkins1" {
+  subnet_id      = "${aws_subnet.jenkins1.id}"
   route_table_id = "${aws_route_table.external.id}"
 }
 
-resource "aws_subnet" "jenkins" {
-  vpc_id = "${aws_vpc.jenkins.id}"
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "${var.availability_zone}"
+resource "aws_route_table_association" "external-jenkins2" {
+  subnet_id      = "${aws_subnet.jenkins2.id}"
+  route_table_id = "${aws_route_table.external.id}"
+}
+
+resource "aws_subnet" "jenkins1" {
+  vpc_id            = "${aws_vpc.jenkins.id}"
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-west-2a"            // need to gget from the list var.availability_zones
 
   tags {
-    for = "${var.ecs_cluster_name}"
+    Name = "${var.ecs_cluster_name}-subnet1"
+  }
+}
+
+resource "aws_subnet" "jenkins2" {
+  vpc_id            = "${aws_vpc.jenkins.id}"
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-2b"            // need to gget from the list var.availability_zones
+
+  tags {
+    for = "${var.ecs_cluster_name}-subnet2"
   }
 }
 
@@ -58,48 +78,62 @@ resource "aws_internet_gateway" "jenkins" {
 }
 
 resource "aws_security_group" "sg_jenkins" {
-  name = "sg_${var.ecs_cluster_name}"
+  name        = "sg_${var.ecs_cluster_name}"
   description = "Allows all traffic"
-  vpc_id = "${aws_vpc.jenkins.id}"
+  vpc_id      = "${aws_vpc.jenkins.id}"
+
+  tags {
+    Name = "ecs-jenkins-sg"
+  }
 
   ingress {
     from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    to_port   = 22
+    protocol  = "tcp"
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
   }
 
   ingress {
     from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    to_port   = 80
+    protocol  = "tcp"
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
   }
 
   ingress {
     from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    to_port   = 443
+    protocol  = "tcp"
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
   }
 
   ingress {
     from_port = 50000
-    to_port = 50000
-    protocol = "tcp"
+    to_port   = 50000
+    protocol  = "tcp"
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
   }
 
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
+
     cidr_blocks = [
-      "0.0.0.0/0"]
+      "0.0.0.0/0",
+    ]
   }
 }
 
@@ -108,48 +142,53 @@ resource "aws_ecs_cluster" "jenkins" {
 }
 
 resource "aws_autoscaling_group" "asg_jenkins" {
-  name = "asg_${var.ecs_cluster_name}"
-  availability_zones = ["${var.availability_zone}"]
-  min_size = "${var.min_instance_size}"
-  max_size = "${var.max_instance_size}"
-  desired_capacity = "${var.desired_instance_capacity}"
-  health_check_type = "EC2"
+  name                      = "asg_${var.ecs_cluster_name}"
+  availability_zones        = ["${var.availability_zones}"]
+  min_size                  = "${var.min_instance_size}"
+  max_size                  = "${var.max_instance_size}"
+  desired_capacity          = "${var.desired_instance_capacity}"
+  health_check_type         = "EC2"
   health_check_grace_period = 300
-  launch_configuration = "${aws_launch_configuration.lc_jenkins.name}"
-  vpc_zone_identifier = ["${aws_subnet.jenkins.id}"]
+  launch_configuration      = "${aws_launch_configuration.lc_jenkins.name}"
+  vpc_zone_identifier       = ["${aws_subnet.jenkins1.id}", "${aws_subnet.jenkins2.id}"]
 
   lifecycle {
     create_before_destroy = true
   }
 
   tag {
-    key = "Name"
-    value = "${var.ecs_cluster_name}_asg"
+    key                 = "Name"
+    value               = "${var.ecs_cluster_name}_asg"
     propagate_at_launch = true
   }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_jenkins" {
+  autoscaling_group_name = "${aws_autoscaling_group.asg_jenkins.id}"
+  alb_target_group_arn   = "${aws_alb_target_group.ecs-target-group.arn}"
 }
 
 data "template_file" "user_data" {
   template = "${file("templates/user_data.tpl")}"
 
   vars {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    s3_bucket = "${var.s3_bucket}"
+    access_key       = "${var.access_key}"
+    secret_key       = "${var.secret_key}"
+    s3_bucket        = "${var.s3_bucket}"
     ecs_cluster_name = "${var.ecs_cluster_name}"
-    restore_backup = "${var.restore_backup}"
+    restore_backup   = "${var.restore_backup}"
   }
 }
 
 resource "aws_launch_configuration" "lc_jenkins" {
-  name_prefix = "lc_${var.ecs_cluster_name}-"
-  image_id = "${lookup(var.amis, var.region)}"
-  instance_type = "${var.instance_type}"
-  security_groups = ["${aws_security_group.sg_jenkins.id}"]
-  iam_instance_profile = "${aws_iam_instance_profile.iam_instance_profile.name}"
-  key_name = "${var.key_name}"
+  name_prefix                 = "lc_${var.ecs_cluster_name}-"
+  image_id                    = "${lookup(var.amis, var.region)}"
+  instance_type               = "${var.instance_type}"
+  security_groups             = ["${aws_security_group.sg_jenkins.id}"]
+  iam_instance_profile        = "${aws_iam_instance_profile.iam_instance_profile.name}"
+  key_name                    = "${var.key_name}"
   associate_public_ip_address = true
-  user_data = "${data.template_file.user_data.rendered}"
+  user_data                   = "${data.template_file.user_data.rendered}"
 
   lifecycle {
     create_before_destroy = true
@@ -157,18 +196,73 @@ resource "aws_launch_configuration" "lc_jenkins" {
 }
 
 resource "aws_iam_role" "host_role_jenkins" {
-  name = "host_role_${var.ecs_cluster_name}"
+  name               = "host_role_${var.ecs_cluster_name}"
   assume_role_policy = "${file("policies/ecs-role.json")}"
 }
 
 resource "aws_iam_role_policy" "instance_role_policy_jenkins" {
-  name = "instance_role_policy_${var.ecs_cluster_name}"
+  name   = "instance_role_policy_${var.ecs_cluster_name}"
   policy = "${file("policies/ecs-instance-role-policy.json")}"
-  role = "${aws_iam_role.host_role_jenkins.id}"
+  role   = "${aws_iam_role.host_role_jenkins.id}"
 }
 
 resource "aws_iam_instance_profile" "iam_instance_profile" {
   name = "iam_instance_profile_${var.ecs_cluster_name}"
   path = "/"
   role = "${aws_iam_role.host_role_jenkins.name}"
+}
+
+resource "aws_alb" "alb_jenkins" {
+  name            = "ecs-load-balancer"
+  security_groups = ["${aws_security_group.sg_jenkins.id}"]
+  subnets         = ["${aws_subnet.jenkins1.id}", "${aws_subnet.jenkins2.id}"]
+
+  tags {
+    Name = "ecs-load-balancer"
+  }
+}
+
+resource "aws_alb_target_group" "ecs-target-group" {
+  name     = "ecs-target-group"
+  port     = "80"
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.jenkins.id}"
+
+  health_check {
+    healthy_threshold   = "5"
+    unhealthy_threshold = "2"
+    interval            = "30"
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = "5"
+  }
+
+  tags {
+    Name = "ecs-target-group"
+  }
+}
+
+resource "aws_alb_listener" "alb-listener" {
+  load_balancer_arn = "${aws_alb.alb_jenkins.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_alb_target_group.ecs-target-group.arn}"
+    type             = "forward"
+  }
+}
+
+output "aws_alb_id" {
+  value = "${aws_alb.alb_jenkins.id}"
+}
+
+resource "aws_route53_record" "r53_jenkins" {
+  zone_id = "${var.dns_zone_id}"
+  name    = "jenkins.${var.dns_root}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_alb.alb_jenkins.dns_name}"]
 }
--- variables.tf
+++ variables.tf
@@ -8,27 +8,43 @@ variable "secret_key" {
 
 variable "region" {
   description = "The AWS region to create resources in."
-  default = "us-east-1"
+  default     = "eu-west-2"
 }
 
-variable "availability_zone" {
+variable "profile" {
+  description = "The AWS profile."
+  default     = "mytf"
+}
+
+variable "dns_zone_id" {
+  description = "The AWS identifier for the hosted zone to add names to"
+}
+
+variable "dns_root" {
+  description = "The domain or subdomain to create names in"
+  default     = "aws.example.com"
+}
+
+variable "availability_zones" {
   description = "The availability zone"
-  default = "us-east-1b"
+  default     = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "ecs_cluster_name" {
   description = "The name of the Amazon ECS cluster."
-  default = "jenkins"
+  default     = "jenkins"
 }
 
 variable "amis" {
   description = "Which AMI to spawn. Defaults to the AWS ECS optimized images."
+
   default = {
-    us-east-1 = "ami-8f7687e2"
-    us-west-1 = "ami-bb473cdb"
-    us-west-2 = "ami-84b44de4"
-    eu-west-1 = "ami-4e6ffe3d"
-    eu-central-1 = "ami-b0cc23df"
+    us-east-1      = "ami-8f7687e2"
+    us-west-1      = "ami-bb473cdb"
+    us-west-2      = "ami-84b44de4"
+    eu-west-1      = "ami-4e6ffe3d"
+    eu-west-2      = "ami-2218f945"
+    eu-central-1   = "ami-b0cc23df"
     ap-northeast-1 = "ami-095dbf68"
     ap-southeast-1 = "ami-cf03d2ac"
     ap-southeast-2 = "ami-697a540a"
@@ -36,45 +52,45 @@ variable "amis" {
 }
 
 variable "instance_type" {
-  default = "t2.medium"
+  default = "t2.micro"
 }
 
 variable "key_name" {
-  default = "devops-tf"
+  default     = "myawskey"
   description = "SSH key name in your AWS account for AWS instances."
 }
 
 variable "min_instance_size" {
-  default = 1
+  default     = 1
   description = "Minimum number of EC2 instances."
 }
 
 variable "max_instance_size" {
-  default = 2
+  default     = 2
   description = "Maximum number of EC2 instances."
 }
 
 variable "desired_instance_capacity" {
-  default = 1
+  default     = 1
   description = "Desired number of EC2 instances."
 }
 
 variable "desired_service_count" {
-  default = 1
+  default     = 1
   description = "Desired number of ECS services."
 }
 
 variable "s3_bucket" {
-  default = "mycompany-jenkins"
+  default     = "mycompany-jenkins"
   description = "S3 bucket where remote state and Jenkins data will be stored."
 }
 
 variable "restore_backup" {
-  default = false
+  default     = false
   description = "Whether or not to restore Jenkins backup."
 }
 
 variable "jenkins_repository_url" {
-  default = "jenkins"
+  default     = "jenkins"
   description = "ECR Repository for Jenkins."
 }
